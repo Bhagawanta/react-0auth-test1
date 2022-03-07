d0e706a7f2052d857984da233977207052a7aba8
Bhagwanta <Bhagwanta.Y@e-zest.in>
2022-03-07 18:55:16 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { GoogleLogin } from "react-google-login";
+
+import "./App.css";
+
+const responseGoogle = (response) => {
+  console.log(response);
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Gogle Oauth</h1>
+      <h2>Login with google</h2>
+
+      <GoogleLogin
+        clientId="669907935099-4l8frdlfpv1j82eajmomjv4uvlqp5ade.apps.googleusercontent.com"
+        buttonText="Login"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
     </div>
   );
 }
